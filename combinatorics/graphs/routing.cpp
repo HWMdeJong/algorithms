f5bd7223625321b3e4380e9e5bb3c610f7d73ccf
Arthelais <julianneeleman@hotmail.com>
2015-10-17 14:34:58 +0200

    floyd-warshall added

--- combinatorics/graphs/routing.cpp
+++ combinatorics/graphs/routing.cpp
@@ -4,8 +4,8 @@
 
 using namespace std;
 
-vector<int64_t> dijkstra(int64_t source, vector<vector<int64_t> > adjacency_matrix) {
-	int64_t n = adjacency_matrix.size();
+vector<int64_t> dijkstra(int64_t source, vector<vector<int64_t> > distance_matrix) {
+	int64_t n = distance_matrix.size();
 	vector<int64_t> distances(n, numeric_limits<int64_t>::infinity());
 	distances[source] = 0;
 	vector<bool> visited(n, false);
@@ -22,11 +22,25 @@ vector<int64_t> dijkstra(int64_t source, vector<vector<int64_t> > adjacency_matr
 
 		visited[current] = true;
 		for(int64_t j = 0; j < n; j++) {
-			int64_t path = distances[current] + adjacency_matrix[current][j];
+			int64_t path = distances[current] + distance_matrix[current][j];
 			distances[j] = min(path, distances[j]);
 		}
 	}
 	return distances;
+}
+
+vector<vector<int64_t> > floyd_warshall(vector<vector<int64_t> > distance_matrix) {
+	int64_t n = distance_matrix.size();
+	for(int64_t i = 0; i < n; i++) {
+		for(int64_t j = 0; j < n; j++) {
+			for(int64_t k = 0; k < n; k++) {
+				distance_matrix[i][k] = min(distance_matrix[i][k],
+											distance_matrix[i][j] +
+											distance_matrix[j][k]);
+			}
+		}
+	}
+	return distance_matrix;
 }
 
 int main() {
